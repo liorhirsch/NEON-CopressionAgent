040476f45c986c82b2f188e110748ddfd7ae2fc7
Lior Hirsch <liorhirsch@gmail.com>
2020-07-07 18:45:45 +0300

    change the build of test_name

--- ‏‏A2C_Agent_Reinforce_Runner.py
+++ ‏‏A2C_Agent_Reinforce_Runner.py
@@ -186,7 +186,7 @@ def extract_args_from_cmd():
 
 if __name__ == "__main__":
     args = extract_args_from_cmd()
-    test_name = f'Agent_Combined_{args.dataset_name}_learn_new_layers_only_{args.learn_new_layers_only}_acc_reduction_{args.allowed_reduction_acc}'
+    test_name = f'Agent_{args.dataset_name}_learn_new_layers_only_{args.learn_new_layers_only}_acc_reduction_{args.allowed_reduction_acc}'
     main(dataset_name=args.dataset_name, is_learn_new_layers_only=args.learn_new_layers_only,test_name=test_name,
          is_to_split_cv=args.split,
          total_allowed_accuracy_reduction=args.allowed_reduction_acc)
