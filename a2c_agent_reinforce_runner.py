a572d93081a875b035f8ca1115950609d7b32f32
Lior Hirsch <liorhirsch@gmail.com>
2020-07-26 23:23:17 +0300

    add `with_loops` to tile

--- a2c_agent_reinforce_runner.py
+++ a2c_agent_reinforce_runner.py
@@ -188,7 +188,8 @@ def extract_args_from_cmd():
 
 if __name__ == "__main__":
     args = extract_args_from_cmd()
-    test_name = f'Agent_{args.dataset_name}_learn_new_layers_only_{args.learn_new_layers_only}_acc_reduction_{args.allowed_reduction_acc}'
+    with_loops = '_with_loop' if args.can_do_more_then_one_loop else ""
+    test_name = f'Agent_{args.dataset_name}_learn_new_layers_only_{args.learn_new_layers_only}_acc_reduction_{args.allowed_reduction_acc}{with_loops}'
     main(dataset_name=args.dataset_name, is_learn_new_layers_only=args.learn_new_layers_only,test_name=test_name,
          is_to_split_cv=args.split,
          total_allowed_accuracy_reduction=args.allowed_reduction_acc,
